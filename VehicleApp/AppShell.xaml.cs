829a703c24af476e4195e7f6c7ef66c88ec28cb6
Petar Lenart <petar.lenart@gmail.com>
2023-02-10 11:29:59 +0100

    initial commit

---
+++ VehicleApp/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace VehicleApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
